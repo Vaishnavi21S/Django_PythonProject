a05bb7cfd3fcc00033768c98ad3c623fe6149fa7
Vaishnavi21S <vaishnavisenthilsvgmgmail.com>
2025-06-09 20:05:15 +0530

    Track recipe_project directory

---
+++ recipe_project/recipes/models.py
@@ -0,0 +1,24 @@
+
+from django.db import models
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=200)
+    instructions = models.TextField()
+    servings = models.PositiveIntegerField(default=1)
+    ingredients = models.JSONField(default=list)
+    image = models.ImageField(upload_to='recipe_images/', blank=True, null=True)
+   
+
+    def __str__(self):
+        return self.name
+
+    def adjust_ingredients_for_servings(self, servings):
+        adjusted_ingredients = []
+        for ingredient in self.ingredients:
+            adjusted_ingredient = ingredient.copy()
+            try:
+                adjusted_ingredient['quantity'] = float(ingredient['quantity']) * (servings / self.servings)
+            except (ValueError, TypeError):
+                adjusted_ingredient['quantity'] = 0
+            adjusted_ingredients.append(adjusted_ingredient)
+        return adjusted_ingredients
